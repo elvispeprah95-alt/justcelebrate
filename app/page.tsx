52765ae4df56ae2826f23b2cbf05e2dab014120e
valalberto01-wq <valalberto01@gmail.com>
2026-08-24 19:58:39 +0100

    Render vendor directory on homepage

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,13 @@
 import Header from "./Header";
 import Hero from "./Hero";
+import VendorDirectory from "./VendorDirectory";
 
 export default function Home() {
   return (
     <>
       <Header />
       <Hero />
+      <VendorDirectory />
     </>
   );
 }
