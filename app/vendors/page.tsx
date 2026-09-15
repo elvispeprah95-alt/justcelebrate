bfaec80ba232c927fbe0e220df6fede076cdb5fb
elvispeprah95-alt <elvispeprah95@gmail.com>
2026-09-15 16:35:18 +0100

    Add planner vendor browsing route

---
+++ app/vendors/page.tsx
@@ -0,0 +1,11 @@
+import Header from "../Header";
+import VendorDirectory from "../VendorDirectory";
+
+export default function VendorsPage() {
+  return (
+    <main className="min-h-screen bg-[#f8f4ec]">
+      <Header />
+      <VendorDirectory />
+    </main>
+  );
+}
